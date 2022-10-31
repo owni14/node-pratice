d49cca5deae398a0f27b7547b3a49f82e87854c4
owni14 <kmwoo514@gmail.com>
2022-10-31 15:42:12 +0900

    회원가입 요청 구현

---
+++ app/src/public/js/home/register.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const id = document.querySelector('#id'),
+  name = document.querySelector('#name'),
+  password = document.querySelector('#password'),
+  confirmPassword = document.querySelector('#confirm-password'),
+  registerBtn = document.querySelector('#button');
+
+registerBtn.addEventListener('click', register);
+
+function register() {
+  const req = {
+    id: id.value,
+    name: name.value,
+    password: password.value,
+    confirmPassowrd: confirmPassword.value,
+  };
+  console.log(req);
+
+  console.log(req);
+  console.log(JSON.stringify(req));
+
+  fetch('/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(req),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.success) {
+        location.href = '/login';
+      } else {
+        alert(res.msg);
+      }
+    })
+    .catch((err) => {
+      console.error(new Error('회원가입 중 에러 발생'));
+    });
+}
